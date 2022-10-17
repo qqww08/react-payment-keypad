f446c6a91c094d0e0b54d46649875797f7ef700a
Dan <qqwwee08@naver.com>
2022-10-17 20:00:04 +0900

    fix: build error

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export type { ReactPaymentKeypadProps } from "./types";
+
+export * from "./lib/hooks/usePaymentOpener";
+export * from "./lib/ReactPaymentKeypad";
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-// export type { ReactPaymentKeypadProps } from "./types";
-//
-// export * from "./lib/hooks/usePaymentOpener";
-// export * from "./lib/ReactPaymentKeypad";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-
-ReactDOM.render(<App />, document.getElementById("root"));
